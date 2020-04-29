f24c7a4750b67098bb178fcc80d58f31abd8f8a5
KurobutaU <kurobutau@192.168.254.139>
2020-04-29 17:43:36 +0700

    update code

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,26 @@
 <template>
   <div id="app">
-    <div class="add-ticket-wrapper">
+    <div class="ticketWrapper">
       <form @submit.prevent="onSubmit">
         <div>
-          <label for="">Type</label>
-          <input type="text" v-model="form.type">
+          <label class="colLabel" for="">Type</label>
+          <input type="text" ref="type" v-model="form.type">
         </div>
         <div>
-          <label for="">description</label>
-          <input type="text" v-model="form.description">
+          <label class="colLabel" for="">Description</label>
+          <input type="text" ref="description" v-model="form.description">
         </div>
          <div>
-          <label for="">fine</label>
-          <input type="text" v-model="form.fine">
+          <label class="colLabel" for="">Fine</label>
+          <input type="text" ref="fine" v-model="form.fine">
         </div>
         <div>
-          <button v-on:click="addTickets">Add</button>
+          <label class="colLabel" for="">Paid</label>
+          <input type="radio" id="paid" v-model="form.isPaid" value="true" ref="paid">Yes
+          <input type="radio" id="paid" v-model="form.isPaid" value="false" ref="unpaid" >No
+        </div>
+        <div>
+          <button v-on:click="addTickets">{{btnFromAction}}</button>
         </div>
       </form>
     </div>
@@ -35,10 +40,11 @@
             <td>{{ticket.type}}</td>
             <td>{{ticket.description}}</td>
             <td>{{ticket.fine}}</td>
-            <td>{{ticket.isPaid}}</td>
+            <td>{{ticket.isPaid ? "Yes" : "No"}}</td>
             <td>
-              <button v-on:click="editTicket">edit</button>
-              <button v-on:click="deleteTicket">delete</button>
+              <button v-on:click="paidTicket(index)">{{ticket.isPaid ? "UnPaid":"Paid"}}</button>
+              <button v-on:click="editTicket(index)">edit</button>
+              <button v-on:click="deleteTicket(index)">delete</button>
             </td>
           </tr>
         </tbody>
@@ -55,6 +61,7 @@ export default {
   data:function()
   {
     return{
+      btnFromAction :'Add',
       form:{
         type:'',
         description : '',
@@ -66,27 +73,70 @@ export default {
     }
   },
   methods : {
+    onSubmit(){},
     addTickets() {
-      const ticket ={
-        type:this.form.type,
-        description:this.form.description,
-        fine:this.form.fine,
-        isPaid:false
+      
+      if(this.form.type==="")
+      {
+        alert('please enter type')
+        this.$refs.type.focus()
       }
-      this.tickets=[ ...this.tickets,ticket]
-      this.resetForm()
+      else if(this.form.description==="")
+      {
+        alert('please enter description')
+        this.$refs.description.focus()
+      }
+      else if(this.form.fine==="")
+      {
+        alert('please enter fine')
+        this.$refs.fine.focus()
+      }
+      else
+      {
+        const ticket ={
+          type:this.form.type,
+          description:this.form.description,
+          fine:this.form.fine,
+          isPaid:this.form.isPaid==="true"
+        }
+        
+        this.tickets=[ ...this.tickets,ticket]
+        this.resetForm()
+      }
     },
     resetForm() {
       this.form.type='',
       this.form.description=''
       this.form.fine=''
+      this.btnFromAction='Add'
+      this.$refs.paid.checked=false;
+      this.$refs.unpaid.checked=false;
     },
-    editTicket(){
-      alert('edit')
+    editTicket(index){
+      const ticketEdit = this.tickets[index]
+      this.form.type = ticketEdit.type
+      this.form.description=ticketEdit.description
+      this.form.fine = ticketEdit.fine
+      this.btnFromAction='Update'
+      if(ticketEdit.isPaid)
+      {
+        this.$refs.paid.checked=true;
+      }
+      else{
+        this.$refs.unpaid.checked=true;
+      }
+      this.deleteTicketItem(index)
     },
-    deleteTicket()
+    paidTicket(index){
+      this.tickets[index].isPaid=!this.tickets[index].isPaid;
+    },
+    deleteTicket(index)
     {
-      alert('delete')
+     this.deleteTicketItem(index)
+    },
+    deleteTicketItem(index)
+    {
+      this.tickets.splice(index,1)
     }
   }
 }
@@ -100,5 +150,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.ticketWrapper
+{
+  text-align: left;
 }
 </style>
