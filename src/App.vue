c7ce62e921bc448d70c70b74929f29154666babe
KurobutaU <kurobutau@192.168.254.139>
2020-04-29 16:06:16 +0700

    init file

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,93 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="add-ticket-wrapper">
+      <form @submit.prevent="onSubmit">
+        <div>
+          <label for="">Type</label>
+          <input type="text" v-model="form.type">
+        </div>
+        <div>
+          <label for="">description</label>
+          <input type="text" v-model="form.description">
+        </div>
+         <div>
+          <label for="">fine</label>
+          <input type="text" v-model="form.fine">
+        </div>
+        <div>
+          <button v-on:click="addTickets">Add</button>
+        </div>
+      </form>
+    </div>
+    <div class="tickets-wrapper">
+      <table border="1">
+        <thead>
+          <tr>
+            <th>type</th>
+            <th>description</th>
+            <th>fine</th>
+            <th>paid</th>
+            <th>aciton</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(ticket,index) in tickets" :key="index">
+            <td>{{ticket.type}}</td>
+            <td>{{ticket.description}}</td>
+            <td>{{ticket.fine}}</td>
+            <td>{{ticket.isPaid}}</td>
+            <td>
+              <button v-on:click="editTicket">edit</button>
+              <button v-on:click="deleteTicket">delete</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data:function()
+  {
+    return{
+      form:{
+        type:'',
+        description : '',
+        fine:0,
+        isPaid : false
+      },
+      tickets:[
+      ],
+    }
+  },
+  methods : {
+    addTickets() {
+      const ticket ={
+        type:this.form.type,
+        description:this.form.description,
+        fine:this.form.fine,
+        isPaid:false
+      }
+      this.tickets=[ ...this.tickets,ticket]
+      this.resetForm()
+    },
+    resetForm() {
+      this.form.type='',
+      this.form.description=''
+      this.form.fine=''
+    },
+    editTicket(){
+      alert('edit')
+    },
+    deleteTicket()
+    {
+      alert('delete')
+    }
   }
 }
 </script>
